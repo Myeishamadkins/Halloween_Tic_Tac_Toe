e00664115fce0b3c6d565f430deb4585734ed75a
Myeishamadkins <40666021+Myeishamadkins@users.noreply.github.com>
2019-03-18 18:10:14 -0500

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,109 @@
+// function setUpGame() {
+//     const board = [];
+//     const boardW = 26;
+//     const boardH = 16;
+//     const boardElement = document.getElementById("board");
+//     for (var i = 0; i < boardH; ++i) {
+//         var row = [];
+//         for (var x = 0; x < boardW; ++x) {
+//             var cell = {};
+//             cell.element = document.createElement("div");
+//             boardElement.appendChild(cell.element);
+//             row.push(cell);
+//         }
+//         board.push(row);
+//     }
+// }
+
+var stepSize = 3,
+    EMPTY = " ",
+    boxes = [],
+    turn = "X",
+    score,
+    moves;
+
+function setUpGame() {
+    var board = document.createElement("table");
+    board.setAttribute("border", 1);
+    board.setAttribute("cellspacing", 0);
+
+    var picker = 1;
+    for (var i = 0; i < stepSize; i++) {
+        var row = document.createElement("tr");
+        board.appendChild(row);
+        for (var j = 0; j < stepSize; j++) {
+            var cell = document.createElement("td");
+            cell.setAttribute("height", 120);
+            cell.setAttribute("width", 120);
+            cell.setAttribute("align", "center");
+            cell.setAttribute("valign", "center");
+            cell.classList.add("col" + j, "row" + i);
+            if (i == j) {
+                cell.classList.add("diagonal0");
+            }
+            if (j == stepSize - i - 1) {
+                cell.classList.add("diagonal1");
+            }
+            cell.picker = picker;
+            cell.addEventListener("click", set);
+            row.appendChild(cell);
+            boxes.push(cell);
+            picker += picker;
+        }
+    }
+
+    document.getElementById("tictactoe").appendChild(board);
+    startNewGame();
+}
+
+function startNewGame() {
+    score = {
+        X: 0,
+        O: 0
+    };
+    moves = 0;
+    turn = "X";
+    boxes.forEach(function(square) {
+        square.innerHTML = EMPTY;
+    });
+}
+
+function win(clicked) {
+    var memberOf = clicked.className.split(/\s+/);
+    for (var i = 0; i < memberOf.length; i++) {
+        var testClass = "." + memberOf[i];
+        var items = contains("#tictactoe " + testClass, turn);
+        if (items.length == stepSize) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function contains(selector, text) {
+    var elements = document.querySelectorAll(selector);
+    return [].filter.call(elements, function(element) {
+        return RegExp(text).test(element.textContent);
+    });
+}
+
+function set() {
+    if (this.innerHTML !== EMPTY) {
+        return;
+    }
+    this.innerHTML = turn;
+    moves += 1;
+    score[turn] += this.picker;
+    if (win(this)) {
+        alert("Winner: Player " + turn);
+        startNewGame();
+    } else if (moves === stepSize * stepSize) {
+        alert("Draw");
+        startNewGame();
+    } else {
+        turn = turn === "X" ? "O" : "X";
+        document.getElementById("turn").textContent = "Player " + turn;
+    }
+}
+
+setUpGame();
